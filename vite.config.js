ccdcb7a9b1d0f0d17486087d8bb5e62f90ea4cc4
A_monster <91659291+I-am-a-monster@users.noreply.github.com>
2026-08-01 03:53:43 +0200

    Update vite base path to support both Netlify and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/celina_wedding/',
+  base: './',
   plugins: [react()],
   server: {
     watch: {
